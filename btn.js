e974caf8dd957a8ba9714b8a2a4a26418a99bad7
Nicolás Flores <110703674+nicolasezequielflores@users.noreply.github.com>
2023-01-17 01:54:56 -0300

    PORTAFOLIO---NEF
    1.0

---
+++ btn.js
@@ -0,0 +1,11 @@
+const button = document.getElementById("back-to-top");
+
+//Agregando evento click
+button.addEventListener("click", () => {
+    //Volver al inicio de la pagina
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+    });
+});
